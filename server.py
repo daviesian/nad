8e58cebd1819947a4d195407a4a368a589e2d388
Ian Davies <git@iandavies.org>
2024-08-20 20:11:15 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,24 @@
 from nad import NadMixer
-
+import os
 import anvil.server
 
 mixer = NadMixer()
 
-@anvil.server.callable
+@anvil.server.callable("amp.reset")
 def reset():
   mixer.on_start()
 
-@anvil.server.callable
+@anvil.server.callable("amp.get_volume")
 def get_volume():
   return mixer.get_volume()
 
-@anvil.server.callable
+@anvil.server.callable("amp.set_volume")
 def set_volume(vol):
   mixer._power_device_on()
   return mixer.set_volume(vol)
 
 
-@anvil.server.callable
+@anvil.server.callable("amp.set_power_on")
 def set_power_on(on):
   if on:
     mixer._power_device_on()
@@ -26,23 +26,23 @@ def set_power_on(on):
     mixer._power_device_off()
   return mixer._ask_device('Main.Power') == "On"
 
-@anvil.server.callable
+@anvil.server.callable("amp.get_power_on")
 def get_power_on():
   return mixer._ask_device('Main.Power') == "On"
 
-@anvil.server.callable
+@anvil.server.callable("amp.set_source")
 def set_source(src):
   if mixer._check_and_set('Main.Source', src):
     return src
   else:
     return mixer._ask_device('Main.Source')
 
-@anvil.server.callable
+@anvil.server.callable("amp.get_source")
 def get_source():
   return mixer._ask_device('Main.Source')
 
 
-anvil.server.connect("P2YI4HVQLPJRUFTW-CIYAMZSFMUNPUFDJ")
+anvil.server.connect(os.getenv("UPLINK_KEY"))
 
 mixer.on_start()
 
